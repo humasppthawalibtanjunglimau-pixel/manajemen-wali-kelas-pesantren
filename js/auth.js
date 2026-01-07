1743f92c3804fc39a460ba997597ed53a543f83b
humasppthawalibtanjunglimau-pixel <humasppthawalibtanjunglimau@gmail.com>
2026-01-07 08:35:41 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,26 @@
-if (role === "wali" && kelas !== kelasLogin) {
-  alert("Akses ditolak");
-}
+function login() {
 
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // DATA USER (contoh)
+  if (username === "admin" && password === "123") {
+    window.location.href = "admin.html";
+  }
+
+  else if (username === "wali7a" && password === "123") {
+    window.location.href = "wali.html";
+  }
+
+  else if (username === "guru" && password === "123") {
+    window.location.href = "guru.html";
+  }
+
+  else if (username === "pimpinan" && password === "123") {
+    window.location.href = "pimpinan.html";
+  }
+
+  else {
+    alert("Username atau password salah");
+  }
+}

11285af96ddcfb39950d7551bee23b3affa1e890
humasppthawalibtanjunglimau-pixel <humasppthawalibtanjunglimau@gmail.com>
2026-01-07 08:42:37 +0700

    Update users.js

--- js/users.js
+++ js/users.js
@@ -1,1 +1,29 @@
+var users = [
+
+  // ADMIN
+  { username: "admin", password: "123", role: "admin" },
+
+  // WALI KELAS (9 ORANG)
+  { username: "wali7a", password: "123", role: "wali", kelas: "VII A" },
+  { username: "wali7b", password: "123", role: "wali", kelas: "VII B" },
+  { username: "wali8a", password: "123", role: "wali", kelas: "VIII A" },
+  { username: "wali8b", password: "123", role: "wali", kelas: "VIII B" },
+  { username: "wali9a", password: "123", role: "wali", kelas: "IX A" },
+  { username: "wali9b", password: "123", role: "wali", kelas: "IX B" },
+  { username: "wali9c", password: "123", role: "wali", kelas: "IX C" },
+  { username: "wali9d", password: "123", role: "wali", kelas: "IX D" },
+  { username: "wali9e", password: "123", role: "wali", kelas: "IX E" },
+
+  // GURU (CONTOH, NANTI BISA DITAMBAH)
+  { username: "guru1", password: "123", role: "guru" },
+  { username: "guru2", password: "123", role: "guru" },
+
+  // PIMPINAN & STRUKTURAL
+  { username: "pimpinan", password: "123", role: "pimpinan" },
+  { username: "kepala1", password: "123", role: "pimpinan" },
+  { username: "kepala2", password: "123", role: "pimpinan" },
+  { username: "wakakur1", password: "123", role: "pimpinan" },
+  { username: "wakakur2", password: "123", role: "pimpinan" }
+
+];
 

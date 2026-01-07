c83ed05981e8e9d21cb35a0169080120e1ac324e
humasppthawalibtanjunglimau-pixel <humasppthawalibtanjunglimau@gmail.com>
2026-01-07 08:52:38 +0700

    Update data-siswa.js

--- js/data-siswa.js
+++ js/data-siswa.js
@@ -1,1 +1,8 @@
+var dataSiswa = [
+  { nama: "Ahmad Fauzi", kelas: "VII A", nilai: 85 },
+  { nama: "Muhammad Rizki", kelas: "VII A", nilai: 88 },
+  { nama: "Aisyah", kelas: "VII B", nilai: 90 },
+  { nama: "Zahra", kelas: "VIII A", nilai: 92 },
+  { nama: "Ali", kelas: "IX A", nilai: 80 }
+];
 

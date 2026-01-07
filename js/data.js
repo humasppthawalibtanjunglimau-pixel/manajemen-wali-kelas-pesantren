cc1c4d25dd888656b92fa17f27d7755a5c5764de
humasppthawalibtanjunglimau-pixel <humasppthawalibtanjunglimau@gmail.com>
2026-01-07 08:46:02 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,10 +1,5 @@
-{
-  nama: "Ahmad",
-  kelas: "VII A",
-  nilai: {
-    akhlak: 85,
-    akademik: 90,
-    kedisiplinan: 88
-  }
-}
-
+var siswa = [
+  { nama: "Ahmad", kelas: "VII A" },
+  { nama: "Budi", kelas: "VII B" },
+  { nama: "Hasan", kelas: "IX E" }
+];
